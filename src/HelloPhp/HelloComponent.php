93c62fb41a6cf93c8cc838cacf3755770d783894
bwinner <bwwiinner@gmail.com>
2019-06-06 16:28:36 +0300

    first commit

---
+++ src/HelloPhp/HelloComponent.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace HelloPhp;
+
+
+class HelloComponent
+{
+    static public function run(){
+        return sprintf("Hello world!");
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use HelloPhp\HelloComponent;
+
+echo HelloComponent::run();
